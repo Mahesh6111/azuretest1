e757b6314467a791e593fd9046e686996dbd041d
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-23 17:50:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "tf-rg" {
 
 resource "azurerm_storage_account" "my-tf-sa" {
   name                     = "storageaccountname1"
-  resource_group_name      = azurerm_resource_group.tf-rg.my-tf-rg
+  resource_group_name      = azurerm_resource_group.tf-rg.name
   location                 = azurerm_resource_group.tf-rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
