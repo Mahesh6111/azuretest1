6ced6e7294a97a20cba13a0775688d0d567268be
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-26 21:36:28 +0530

    Update sa01.tf

--- storageaccounts/sa01.tf
+++ storageaccounts/sa01.tf
@@ -1,18 +1,7 @@
-provider "azurerm" {
-  alias           = "sub01"
-  subscription_id = "3db649d5-9162-430c-ba6d-afeacf2194ec"
-  
-  features {}
-}
-
 module "db01" {
   source                      = "../modules/"
   rg                          = "my-tf-rg"
   location                    = "westeurope"
   storagename                 = "myterraformsa61196"
-  
-  providers = {
-    azurerm = azurerm.sub01
-  }  
-  
+  subscription_id             = "d3b7888f-c26e-4961-a976-ff9d5b31dfd3"
 }
