52646ccc2e6397cd9e5e2ad3987b989d125cc0b3
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-26 13:10:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,9 +9,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "3db649d5-9162-430c-ba6d-afeacf2194ec"
-  client_id       = "96d017e9-894e-40d2-99bd-ad48cbe4ea51"
-  client_secret   = "LZJ8Q~yPSeFQKq1MpUMpyhOBJ0nRFW5r5u7M_c0c"
-  tenant_id       = "54227b23-8c19-44ca-8b05-48d4190f5ef5"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_id
+  tenant_id       = var.tenant_id
 
 }
