4f9ff7b123a29bcfcf5c58b5c8913fd7b4126b3f
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-26 23:58:05 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -8,11 +8,10 @@ terraform {
   }
 }
 
-
 provider "azurerm" {
   features {}
   client_id       = "96d017e9-894e-40d2-99bd-ad48cbe4ea51"
-  client_secret   = "af423ec1-8966-4c87-9557-75bb905b4a81"
+  client_secret   = "LZJ8Q~yPSeFQKq1MpUMpyhOBJ0nRFW5r5u7M_c0c"
   tenant_id       = "54227b23-8c19-44ca-8b05-48d4190f5ef5"
   subscription_id = var.subscription_id
 }
