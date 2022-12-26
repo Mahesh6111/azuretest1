ab620606ce95abf9b80603150b3a4cc559d846f2
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-26 23:32:58 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,13 +1,13 @@
 terraform {
+  backend "remote" {
+    organisation = "maKloud"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3"
-    }
+
   }
 }
 
