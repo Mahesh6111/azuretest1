ed2182f2438e577920a1200ac79775d1644911ca
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-26 13:13:10 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+  description = "subscription id"
+
+}
+
+variable "client_id" {
+  description = "client id"
+
+}
+
+variable "client_secret" {
+  description = "client secret"
+
+}
+
+variable "tenant_id" {
+  description = "tenant id"
+
+}
