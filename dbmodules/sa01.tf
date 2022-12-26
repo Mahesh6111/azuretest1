165d47997f1914e4fac92dcb6549b5043a5999b0
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-26 21:06:43 +0530

    Update sa01.tf

--- dbmodules/sa01.tf
+++ dbmodules/sa01.tf
@@ -1,10 +1,6 @@
 provider "azurerm" {
   features {}
   subscription_id = "3db649d5-9162-430c-ba6d-afeacf2194ec"
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-
 }
 
 module "db01" {
