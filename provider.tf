117895ebbc8066f6307d43f5a5a593f5244e8b51
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-26 13:22:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,9 +9,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  subscription_id = local.subscription_id
+  client_id       = local.client_id
+  client_secret   = local.client_secret
+  tenant_id       = local.tenant_id
 
 }
