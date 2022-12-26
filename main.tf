a2cb592fe5a19fc406405e2c8328e29657b9c179
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-26 19:56:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">=3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "3db649d5-9162-430c-ba6d-afeacf2194ec"
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+
+}
+
+
 resource "azurerm_resource_group" "tf-rg" {
-  name     = "my-tf-rg"
-  location = "West Europe"
+  name     = var.rg
+  location = var.location
 }
 
 resource "azurerm_storage_account" "my-tf-sa1" {
-  name                     = "myterraformsa61196"
+  name                     = var.storagename
   resource_group_name      = azurerm_resource_group.tf-rg.name
   location                 = azurerm_resource_group.tf-rg.location
   account_tier             = "Standard"
