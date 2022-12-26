4ec56d3ce5f9bb01f44ff3d09623a4916ab98931
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-26 21:19:47 +0530

    Create main.tf

---
+++ modules/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "tf-rg" {
+  name     = var.rg
+  location = var.location
+}
+
+resource "azurerm_storage_account" "my-tf-sa1" {
+  name                     = var.storagename
+  resource_group_name      = azurerm_resource_group.tf-rg.name
+  location                 = azurerm_resource_group.tf-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
