72f4ef5e4d7c5214b2149a61a0ff2df0aa509075
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-26 23:58:51 +0530

    Update sa01.tf

--- storageaccounts/sa01.tf
+++ storageaccounts/sa01.tf
@@ -1,7 +1,7 @@
-module "db01" {
+module "sa001" {
   source                      = "../modules"
-  rg                          = "my-tf-rg"
+  rg                          = "my-tf-rg1"
   location                    = "westeurope"
-  storagename                 = "myterraformsa61196"
+  storagename                 = "myterraformsa61196-A"
   subscription_id             = "d3b7888f-c26e-4961-a976-ff9d5b31dfd3"
 }
