a75c11243ac171e711854e57f7e70961df3108cd
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-26 21:16:10 +0530

    Create sa01.tf

---
+++ storageaccounts/sa01.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  alias           = "sub01"
+  subscription_id = "3db649d5-9162-430c-ba6d-afeacf2194ec"
+  features {}
+}
+
+module "db01" {
+  source                      = "../"
+  rg                          = "my-tf-rg"
+  location                    = "westeurope"
+  storagename                 = "myterraformsa61196"
+}
