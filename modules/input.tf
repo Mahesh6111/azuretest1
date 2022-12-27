bd3fb33c2a96a822af7ce529e129a4ab574f2c06
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-27 19:03:50 +0530

    Update input.tf

--- modules/input.tf
+++ modules/input.tf
@@ -16,12 +16,15 @@ variable "subscription_id" {
 
 variable "client_id" {
   description = "client_id"
+  default = "96d017e9-894e-40d2-99bd-ad48cbe4ea51"
 }
 
 variable "client_secret" {
   description = "client_secret"
+  default = "LZJ8Q~yPSeFQKq1MpUMpyhOBJ0nRFW5r5u7M_c0c"
 }
 
 variable "tenant_id" {
   description = "tenant_id"
+  default = "54227b23-8c19-44ca-8b05-48d4190f5ef5"
 }
