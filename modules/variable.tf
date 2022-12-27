b75662e8edacd4e5e8cf501c765192e106592a99
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-27 18:31:28 +0530

    Update variable.tf

--- modules/variable.tf
+++ modules/variable.tf
@@ -9,3 +9,19 @@ variable "location" {
 variable "storagename" {
   description = "Location of Storage account"
 }
+
+variable "subscription_id" {
+  description = "subscription_id"
+}
+
+variable "client_id" {
+  description = "client_id"
+}
+
+variable "client_secret" {
+  description = "client_secret"
+}
+
+variable "tenant_id" {
+  description = "tenant_id"
+}
