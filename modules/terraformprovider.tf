a35e6fc991e66ba294702b494d6cb9f33054e560
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-27 19:18:23 +0530

    Update terraformprovider.tf

--- modules/terraformprovider.tf
+++ modules/terraformprovider.tf
@@ -9,7 +9,9 @@ terraform {
 }
 
 provider "azurerm" {
-  alias           = "sub01"
-  subscription_id = "3db649d5-9162-430c-ba6d-afeacf2194ec"
   features {}
+  
+  subscription_id = "3db649d5-9162-430c-ba6d-afeacf2194ec"
+  alias           = "sub01"
+  
 }
