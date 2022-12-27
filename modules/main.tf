f9e662cd12efc3e12a4a9966f2b48b2f34c46dbc
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-27 15:24:05 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-
-  }
-}
-
-provider "azurerm" {
-  features {}
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  skip_provider_registration = "true"
-}
-
 resource "azurerm_resource_group" "tf-rg" {
   name     = var.rg
   location = var.location
