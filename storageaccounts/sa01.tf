4774eb1f43e73baa8b6656e391fb3c6d1cf016f1
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-27 12:51:36 +0530

    Update sa01.tf

--- storageaccounts/sa01.tf
+++ storageaccounts/sa01.tf
@@ -4,4 +4,7 @@ module "sa001" {
   location                    = "westeurope"
   storagename                 = "myterraformsa61196-A"
   subscription_id             = "d3b7888f-c26e-4961-a976-ff9d5b31dfd3"
+  client_id       = "96d017e9-894e-40d2-99bd-ad48cbe4ea51"
+  client_secret   = "LZJ8Q~yPSeFQKq1MpUMpyhOBJ0nRFW5r5u7M_c0c"
+  tenant_id       = "54227b23-8c19-44ca-8b05-48d4190f5ef5"
 }
