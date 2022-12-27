5875f0869f53f5278145b7fcb500895993e8bd35
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-27 18:35:29 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_storage_account" "my-tf-sa1" {
   location                 = azurerm_resource_group.tf-rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
+  provider                  = azurerm.sub01
   tags = {
     environment = "staging"
   }
