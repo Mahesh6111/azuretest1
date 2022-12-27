ba7be103c90b7c29b73dfdf534b7fec38b04c78f
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-27 18:24:35 +0530

    Update sa01.tf

--- storageaccounts/sa01.tf
+++ storageaccounts/sa01.tf
@@ -1,13 +1,3 @@
-provider "azurerm" {
-  alias           = "sub01"
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = "3db649d5-9162-430c-ba6d-afeacf2194ec"
-  skip_provider_registration = "true"
-  features {}
-}
-
 module "sa001" {
   source                      = "../modules"
   rg                          = "my-tf-rg1"
