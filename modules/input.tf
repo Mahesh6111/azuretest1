b13302deb40153ea2ce0157fdfdbea7019814a3c
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2022-12-27 23:00:10 +0530

    Update input.tf

--- modules/input.tf
+++ modules/input.tf
@@ -12,6 +12,6 @@ variable "storagename" {
 
 variable "subscription01" {
   description = "subscription_id for 01"
-  default = "3db649d5-9162-430c-ba6d-afeacf2194ec"
+#  default = "3db649d5-9162-430c-ba6d-afeacf2194ec"
 }
 
